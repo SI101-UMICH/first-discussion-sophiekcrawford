8d92eb38b7bb6a7d02663ee5040048aa989e6b5e
sophiekcrawford <sophiekc@umich.edu>
2026-01-13 14:31:43 +0000

    Second Commit

--- main.py
+++ main.py
@@ -41,3 +41,7 @@ if __name__ == "__main__":
     movie = get_movie("Inception")
     print(movie)
     print(movie["title"])
+
+    movie = get_movie("La La Land")
+    print(movie)
+    print(movie["title"])
